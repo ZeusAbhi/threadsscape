7bfc6d95488523c8a5449117cbac777f4949f570
Abhinav Tushar <abhinavtata.tushar@gmail.com>
2023-08-16 01:09:29 +0530

    changes

--- app/components/sliderhome.jsx
+++ app/components/sliderhome.jsx
@@ -1,19 +1,72 @@
+// import React from "react";
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "./swiper.css";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import "swiper/css/autoplay";
+
+// // import required modules
+// import { Navigation, Pagination, Autoplay } from "swiper";
+
+// const Sliderhome = () => {
+//   return (
+//     <div className=" xl:h-[70vh] xl:w-[100vw] h-[50vh] w-[32vw]">
+//       <Swiper
+//         pagination={true}
+//         modules={[Navigation, Pagination, Autoplay]}
+//         slidesPerView={1}
+//         autoplay={{
+//           delay: 3000,
+//           pauseOnMouseEnter: true,
+//           disableOnInteraction: false,
+//         }}
+//         speed={800}
+//         loop={true}
+//         className="mySwiper"
+//       >
+//         <SwiperSlide>
+//           <a
+//             href="https://threadsscape.vercel.app/shop/prod_01H40HGA9DRVTRGMWVF1PJQ2TE"
+//             className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+//             style={{ backgroundImage: "url('medusa_1.png')" }}
+//           ></a>
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <a
+//             href="https://threadsscape.vercel.app/shop/prod_01H40HG9ZMR7E9NCXJBDV3QY2E"
+//             className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+//             style={{ backgroundImage: "url('medusa_2.png')" }}
+//           ></a>
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <a
+//             href="https://threadsscape.vercel.app/shop/prod_01H40HGA5P9X6HRQEESBC662KY"
+//             className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+//             style={{ backgroundImage: "url('medusa_3.png')" }}
+//           ></a>
+//         </SwiperSlide>
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default Sliderhome;
 import React from "react";
-// Import Swiper React components
+import Link from "next/link"; // Import Link from Next.js
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./swiper.css";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
-
-// import required modules
 import { Navigation, Pagination, Autoplay } from "swiper";
 
 const Sliderhome = () => {
   return (
-    <div className=" xl:h-[70vh] xl:w-[100vw] h-[50vh] w-[32vw]">
+    <div className="xl:h-[70vh] xl:w-[100vw] h-[50vh] w-[32vw]">
       <Swiper
         pagination={true}
         modules={[Navigation, Pagination, Autoplay]}
@@ -28,25 +81,28 @@ const Sliderhome = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <a
-            href="https://threadsscape.vercel.app/shop/prod_01H40HGA9DRVTRGMWVF1PJQ2TE"
-            className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('medusa_1.png')" }}
-          ></a>
+          <Link href="https://threadsscape.vercel.app/shop/prod_01H40HGA9DRVTRGMWVF1PJQ2TE">
+            <div
+              className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+              style={{ backgroundImage: "url('medusa_1.png')" }}
+            ></div>
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <a
-            href="https://threadsscape.vercel.app/shop/prod_01H40HG9ZMR7E9NCXJBDV3QY2E"
-            className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('medusa_2.png')" }}
-          ></a>
+          <Link href="https://threadsscape.vercel.app/shop/prod_01H40HG9ZMR7E9NCXJBDV3QY2E">
+            <div
+              className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+              style={{ backgroundImage: "url('medusa_2.png')" }}
+            ></div>
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <a
-            href="https://threadsscape.vercel.app/shop/prod_01H40HGA5P9X6HRQEESBC662KY"
-            className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('medusa_3.png')" }}
-          ></a>
+          <Link href="https://threadsscape.vercel.app/shop/prod_01H40HGA5P9X6HRQEESBC662KY">
+            <div
+              className="block xl:h-[70vh] xl:w-[100vw] xl:bg-contain h-[50vh] w-[32vw] border-2 border-white bg-cover bg-center bg-no-repeat"
+              style={{ backgroundImage: "url('medusa_3.png')" }}
+            ></div>
+          </Link>
         </SwiperSlide>
       </Swiper>
     </div>
@@ -54,3 +110,4 @@ const Sliderhome = () => {
 };
 
 export default Sliderhome;
+
